5158034af77a1147afcc6a2d009b010762aaa68a
Antonio Villarreal <tonyjv10@gmail.com>
2024-04-08 19:10:36 -0400

    fixed backend issue

--- docker_stop.sh
+++ docker_stop.sh
@@ -9,11 +9,19 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     docker stop $container_id
     # Remove the container
     docker rm $container_id
+    # Remove the image
+    if [[ "$1" == "--clear" || "$1" == "-c" ]]; then
+        docker rmi $container_id
+    fi
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     # Stop the container
     docker stop classification-app
     # Remove the container
     docker rm classification-app
+    # Remove the image
+    if [[ "$1" == "--clear" || "$1" == "-c" ]]; then
+        docker rmi classification-app
+    fi
 else
     echo "Unsupported operating system"
     exit 1
