198b6548bb2523789e36a500503c5f15a3722987
Antonio Villarreal <tonyjv10@gmail.com>
2024-04-09 14:15:07 -0400

    download

--- application/download.py
+++ application/download.py
@@ -12,18 +12,20 @@ if __name__ == "__main__":
     with open(json_file_path, "r") as json_file:
         model_info = json.load(json_file)
             
-    # Download image_model.h5
-    image_model_id = model_info["image_model"]
-    image_model_url = f"https://drive.google.com/uc?id={image_model_id}"
-    filename_2 = "image_model.h5"
-    destination_2 = os.path.join(filename_2)
-    download_from_google_drive(image_model_url, destination_2)
+    # Download image_model.h5 if it doesn't exist
+    if not os.path.exists("image_model.h5"):
+        image_model_id = model_info["image_model"]
+        image_model_url = f"https://drive.google.com/uc?id={image_model_id}"
+        filename_2 = "image_model.h5"
+        destination_2 = os.path.join(filename_2)
+        download_from_google_drive(image_model_url, destination_2)
 
-    # Download face_model.h5
-    face_model_id = model_info["face_model"]
-    face_model_url = f"https://drive.google.com/uc?id={face_model_id}"
-    filename_1 = "face_model.h5"
-    destination_1 = os.path.join(filename_1)
-    download_from_google_drive(face_model_url, destination_1)
+    # Download face_model.h5 if it doesn't exist
+    if not os.path.exists("face_model.h5"):
+        face_model_id = model_info["face_model"]
+        face_model_url = f"https://drive.google.com/uc?id={face_model_id}"
+        filename_1 = "face_model.h5"
+        destination_1 = os.path.join(filename_1)
+        download_from_google_drive(face_model_url, destination_1)
 
 
--- docker_run.sh
+++ docker_run.sh
@@ -11,12 +11,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     # Run Docker container
     docker run -d -p 5001:5001 -p 8501:8501 --name classification-app classification-app
-    
-    # Wait for the Docker container to finish starting up
-    wait $(docker ps -q --filter "name=classification-app")
 
     # Open localhost:8501
-    sleep 15 && xdg-open http://localhost:8501
+    sleep 30 && xdg-open http://localhost:8501
 
 # Check if the classification-app image exists for Windows
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
@@ -29,7 +26,7 @@ elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     docker run -d -p 5001:5001 -p 8501:8501 --name classification-app classification-app
 
     # Open localhost:8501
-    sleep 45 && start http://localhost:8501
+    sleep 30 && start http://localhost:8501
 
 else
     echo "Unsupported operating system"
