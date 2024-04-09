cdb06c86269bf61fd3cc59f0ebe0eb911d426133
Antonio Villarreal <tonyjv10@gmail.com>
2024-04-09 16:21:50 -0400

    change

--- application/download.py
+++ application/download.py
@@ -2,17 +2,13 @@ import os
 import gdown
 from onedrivedownloader import download
 import json
+import threading
 
 
 def download_from_google_drive(url, output_file):
     gdown.download(url, output_file, quiet=False)
 
-
-if __name__ == "__main__":
-    json_file_path = os.path.join("models.json")
-    with open(json_file_path, "r") as json_file:
-        model_info = json.load(json_file)
-
+def download_image_model(model_info):
     # Download image_model.h5 if it doesn't exist
     if not os.path.exists("image_model.h5"):
         # Attempt Google Drive Download
@@ -29,8 +25,9 @@ if __name__ == "__main__":
             except Exception as e:
                 print("OneDrive download failed:", e)
                 print("Please refer to the README on how to manually download the models.")
+    print("Image Model Download Completed!")
 
-    # Download face_model.h5 if it doesn't exist
+def download_face_model(model_info):
     if not os.path.exists("face_model.h5"):
         # Attempt Google Drive Download
         face_model_id = model_info["face_model"]['google_drive_id']
@@ -46,3 +43,21 @@ if __name__ == "__main__":
             except Exception as e:
                 print("OneDrive download failed:", e)
                 print("Please refer to the README on how to manually download the models.")
+    print("Face Model Download Completed!")
+    
+def main():
+    json_file_path = os.path.join("models.json")
+    with open(json_file_path, "r") as json_file:
+        model_info = json.load(json_file)
+        
+    thread1 = threading.Thread(target=download_face_model, args=(model_info,))
+    thread2 = threading.Thread(target=download_image_model, args=(model_info,))
+    
+    thread1.start()
+    thread2.start()
+    
+    thread1.join()
+    thread2.join()
+
+if __name__ == "__main__":
+    main()
--- docker_run.sh
+++ docker_run.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-set -e
+set -ez
 
 # Check if the classification-app image exists for Linux
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if ! docker images classification-app | grep -q classification-app; then
         # Build Docker image
         docker build -t classification-app application
-
-        while ! docker images classification-app | grep -q classification-app; do
-            sleep 1
-        done
     fi
-    
+
     # Run Docker container
     docker run -d -p 5001:5001 -p 8501:8501 --name classification-app classification-app
 
     # Open localhost:8501
-    sleep 30 && xdg-open http://localhost:8501
+    sleep 45 && xdg-open http://localhost:8501
 
 # Check if the classification-app image exists for Windows
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
@@ -30,7 +26,7 @@ elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     docker run -d -p 5001:5001 -p 8501:8501 --name classification-app classification-app
 
     # Open localhost:8501
-    sleep 30 && start http://localhost:8501
+    sleep 45 && start http://localhost:8501
 
 else
     echo "Unsupported operating system"
